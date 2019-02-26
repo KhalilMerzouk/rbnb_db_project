8b061c48396a21c5b45eb7a66a11ce99c3d93c71
Hédi <hedi.sassi@epfl.ch>
2019-02-26 22:34:54 +0100

    Modifications to take into account line separators inside data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
-import java.io.{BufferedWriter, FileWriter}
+import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
 
+import scala.collection.parallel.ParSeq
 import scala.io.BufferedSource
 
 /**
@@ -7,28 +8,51 @@ import scala.io.BufferedSource
   */
 object Main{
 
+  val separator = System.lineSeparator()
 
   def main(args: Array[String]): Unit = {
 
     println("Main app for cleaning DB")
 
+    //IO Paths
     val filePath = "/home/hedi/Documents/EPFL/sem_6/intro_db/DB_project/dataset/barcelona_listings.csv"
-
     val outputPath = "/home/hedi/Documents/EPFL/sem_6/intro_db/DB_project/cleanedData/barcelona_listings_cleaned.csv"
 
+
+    //IO Objects
     val bufferedSource = io.Source.fromFile(filePath)
     val outputFile = new BufferedWriter(new FileWriter(outputPath))
 
 
+    //concatenate lines that were separated by a line separator inside the data
+    def collectList(s: Stream[String], acc: List[String], counter: Int): List[String] = s match{
 
-    val checkedData = checkListing(bufferedSource)
+      case Stream.Empty => acc
 
+      case h +: Stream.Empty => h :: acc
+
+      case h +: tail =>
+        if(h.split(",").size < 58) collectList((s.head + s.tail.head) +: s.tail.tail, acc, counter + 1)   //count the number of columns to know if the line is really finished
+        else collectList(s.tail, s.head :: acc, 0)
+
+    }
+
+    val stream = bufferedSource.getLines().toStream
+
+    val column = stream.head
+    val data = collectList(stream.tail, Nil,0).par
+
+
+    //Perform checks
+    val checkedData = checkListing(data)
+
+    //Write in file
+    outputFile.write(column + separator)
     checkedData.foreach(s => outputFile.write(s))
 
 
     bufferedSource.close
     outputFile.close()
-
   }
 
 
@@ -37,7 +61,7 @@ object Main{
     * @param b the buffered source from the file
     * @return an iterator on a sequence of strings
     */
-  def checkListing(b: BufferedSource): Iterator[String] = for(line <- b.getLines if(checkEmpty(line))) yield line
+  def checkListing(l : ParSeq[String]): ParSeq[String] = for(line <- l if(checkEmpty(line))) yield line + separator
 
 
   /**
