86fc480e8b35432d804916dc3d134c571e3e4580
Hédi <hedi.sassi@epfl.ch>
2019-03-05 19:20:31 +0100

    new checks added. Now all primary keys are checked and missing values are replaced by NULL

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,12 +3,13 @@ import java.io._
 import com.github.tototoshi.csv.{CSVReader, CSVWriter}
 
 import scala.collection.parallel.ParSeq
-import scala.io.BufferedSource
 
 /**
   * Program to clean up the data
   */
 object Main{
+
+  val nullVal = "NULL"
 
   val separator = System.lineSeparator()
 
@@ -32,17 +33,38 @@ object Main{
 
     val data = dataset.tail.par
 
+    val completedData = putNUll(data)
+
     //Perform checks
-    val checkedData = checkListing(data).toList
+    val checkedData = checkListing(completedData).toList
 
     //Write in file
     writer.writeRow(column)
-    writer.writeAll( checkedData  )
+    writer.writeAll(checkedData)
 
 
    //Close to save ressources
     reader.close
     writer.close()
+  }
+
+
+  /**
+    * Replace missing values (empty strings) by "NULL"
+    * @param seq the sequance of csv lines
+    * @return a sequence of csv lines where missing values are replaced by "NULL"
+    */
+  def putNUll(seq: ParSeq[List[String]]): ParSeq[List[String]] = {
+
+    def replaceInLine(line : List[String]): List[String] ={
+
+      for(data <- line) yield if(data.isEmpty)  nullVal else data
+
+    }
+
+    for(line <- seq
+    )yield replaceInLine(line)
+
   }
 
 
@@ -61,9 +83,34 @@ object Main{
     */
   def check(l: List[String]):Boolean = {
 
+    primaryKeysOK(l) && fieldsFormatOK(l)   //add here all checks that necessitate to DROP the current line
+
+  }
+
+  /**
+    * Check that all primary keys (or mandatory data) are present
+    * @param l the csv line to check
+    * @return true if all the primary keys are present fasle otherwise
+    */
+  def primaryKeysOK(l: List[String]):Boolean = {
+
+    val primaryKeysIndex = List(0, 13)    //add indexes of mandatory columns here
+
+    for(i <- primaryKeysIndex){
+      if(l(i) == nullVal) false
+    }
+
     true
 
   }
 
+  /**
+    * Check that the format of the data is consistent (date, strings, numbers etc...)
+    * @param l the csv line to check
+    * @return true if the data is consistent false otherwise
+    */
+  def fieldsFormatOK(l: List[String]):Boolean = {
+    true
+  }
 
 }
